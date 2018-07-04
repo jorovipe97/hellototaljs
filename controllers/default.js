822f2a16add537369ebf900cf20d91f6e086577f
unknown <jorovipe@gmail.com>
2018-07-04 14:29:13 -0500

    First hello world

--- controllers/default.js
+++ controllers/default.js
@@ -1,5 +1,7 @@
 exports.install = function() {
 	F.route('/', view_index);
+
+	F.route('/hello', view_hello);
 	// or
 	// F.route('/');
 };
@@ -7,4 +9,9 @@ exports.install = function() {
 function view_index() {
 	var self = this;
 	self.view('index');
+}
+
+function view_hello() {
+	var self = this;
+	self.view('hello');
 }
